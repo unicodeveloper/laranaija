05473f736e150f265f9dd45c954312a710a74c06
busayo <prosperotemuyiwa@gmail.com>
2014-12-05 12:21:40 +0100

    host name value changed

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment( function(){
 	
-	return gethostname() == 'laranaija.com' ? 'production' : 'development';
+	return gethostname() == 'snowy-chasm' ? 'production' : 'development';
 
 });
 
