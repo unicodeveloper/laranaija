35ca909245cd45f8b21bcaa2d196a25d9c2fd5c8
busayo <prosperotemuyiwa@gmail.com>
2014-12-05 09:32:36 +0100

    dev config updated

--- app/config/development/database.php
+++ app/config/development/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'localhost',
+			'host'      => 'forge',
 			'database'  => 'laranaija',
-			'username'  => 'root',
-			'password'  => '',
+			'username'  => 'forge',
+			'password'  => 'WvMToqKJZFRPxYlgZYmv',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
