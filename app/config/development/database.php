c0af42f2807d776e05e3fe7a279c8c2ff817231d
busayo <prosperotemuyiwa@gmail.com>
2014-12-05 09:36:19 +0100

    Config of dev and production updated

--- app/config/development/database.php
+++ app/config/development/database.php
@@ -22,10 +22,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => 'forge',
-			'database'  => 'laranaija',
-			'username'  => 'forge',
-			'password'  => 'WvMToqKJZFRPxYlgZYmv',
+			'host'      => $_ENV[ DB_HOST ],
+			'database'  => $_ENV[ DB_NAME ],
+			'username'  => $_ENV[ DB_USERNAME ],,
+			'password'  => $_ENV[ DB_PASSWORD ],,
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
