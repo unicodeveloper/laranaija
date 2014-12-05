fb06f28ca4194808993b322286141d652274651c
busayo <prosperotemuyiwa@gmail.com>
2014-12-05 09:46:05 +0100

    Config values updated

--- app/config/database.php
+++ app/config/database.php
@@ -54,10 +54,10 @@ return array(
 
 		'mysql' => array(
 			'driver'    => 'mysql',
-			'host'      => $_ENV[ 'DB_HOST' ],
-			'database'  => $_ENV[ 'DB_NAME' ],
-			'username'  => $_ENV[ 'DB_USERNAME' ],
-			'password'  => $_ENV[ 'DB_PASSWORD' ],
+			'host'      => '104.237.134.141',
+			'database'  => 'laranaija',
+			'username'  => 'forge',
+			'password'  => 'WvMToqKJZFRPxYlgZYmv',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
