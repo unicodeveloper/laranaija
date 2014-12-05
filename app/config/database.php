9c8db92875280543f958b7f82878f7d0a88e6b9d
busayo <prosperotemuyiwa@gmail.com>
2014-12-05 09:11:04 +0100

    Database config values updated

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
+			'database'  => 'laranaija',
 			'username'  => 'forge',
-			'password'  => 'ddfdfdfdf',
+			'password'  => 'WvMToqKJZFRPxYlgZYmv',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
