eba8ff3c5eb2c732a5000e03508acdd1076bf0a4
busayo <prosperotemuyiwa@gmail.com>
2015-08-12 09:18:53 +0100

    Fix redirect url

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -6,7 +6,7 @@
     <div class="row">
       <h2 class="big-title">THIS IS ACTUALLY LARANAIJA 404 ERROR PAGE</h2>
       <h4 class="sub-title">DOESNT EXIST OOOOOOOO</h4>
-      <a class="call-to-action" href="http://devcenter.co/feed">GO BACK TO THE HOMEPAGE</a>
+      <a class="call-to-action" href="http://laranaija.com">GO BACK TO THE HOMEPAGE</a>
     </div>
   </div>
 </div>
